209c5347fbaf02285d629f01defcf7e0a45c255f
John Guanlao <John.Guanlao@seattle.gov>
2017-03-09 09:04:02 -0800

    Updates
    -Buttons are now consistent with application style standards
    -Removed “Please” from label/prompts
    -Warning thrown if step 3 is more than step 2
    -Hitting enter proceeds to next step
    -Help icon changed to “Find this on Bill”
    -Progress bar colors changed to avoid conflict with secondary nav

--- script.js
+++ script.js
@@ -12,19 +12,12 @@ $6.01 x 44 dos =  $264.71 (this is what a tenant would owe if they moved 3/31/16
 */
 
 $(function(){
-	var totalDue;
-	var serviceDays;
-	var partialDays;
-	var costPerDay;
-	var partialDue;
-
 	$('.helpModal, .helpModal button').click(function() {
 		$('.helpModal').fadeOut();
 	});
-
 	$('.step button.help').click(function() {
 		var step = $(this).closest('.step').attr('id');
-		console.log(step);
+		//console.log(step);
 		switch (step){
 			case 'step1':
 				$('.helpModal .step2').hide();
@@ -37,45 +30,44 @@ $(function(){
 		}
 		$('.helpModal').fadeIn();
 	});
-
 	$('#totalDue').focusout(function(){
 		var userInput = parseFloat( $(this).val() ) || 0.0;
 		$(this).val(userInput.toFixed(2));
 	});
-
 	$('.step button.next').click(function() {
 		var $step = $(this).closest('.step');
 		var input = parseFloat($step.find('input').val()) || 0.0;
 		var	$error = $step.find('.errorWrapper');
 		var progressPosition = ($step.index() + 1) * 25;
-
 		//console.log(input);
-	
 		if (input && input !== 0.0 || $step[0].id === 'stepWelcome') {
 			$error.fadeOut();
-
-			if ($step[0].id === 'step3') {
-				$('.amountDue').text('$ ' + amountDue());
+			if ($step[0].id === 'step3' && input >= $('#serviceDays').val() ) {
+				$error.find('.errorMessage')
+						.text('A tenant\'s stay must be less than or equal to the total days of service');
+				$error.fadeIn();
+			} else {
+				if ($step[0].id === 'step3') {
+					$('.amountDue').text('$ ' + amountDue());
+				}
+				$('.step').removeClass('active');
+				$('.progressBar .step:eq('+ ($step.index() + 1) +')').addClass('active');
+				$step.next('.step').toggle('slide', {direction:'right'}, 300);
+				$step.toggle('slide', {direction:'left'}, 300); 
+				$('.stepIndicator').css('left', progressPosition + '%');
 			}
-			$('.step').removeClass('active');
-			$('.progressBar .step:eq('+ ($step.index() + 1) +')').addClass('active');
-			$step.next('.step').toggle('slide', {direction:'right'}, 300);
-			$step.toggle('slide', {direction:'left'}, 300); 
-			$('.stepIndicator').css('left', progressPosition + '%');
 		} else {
-			console.log('yo, you need input');
+			//console.log('yo, you need input');
 			$error.find('.errorMessage')
 					.text('You must enter a number greater than 0.');
 			$error.fadeIn();
 		}
 	});	
-
 	$('.step button.previous').click(function() {
 		var $parentStep = $(this).closest('.step');
 		$parentStep.prev('.step').toggle('slide');
 		$parentStep.toggle('slide');
 	});		
-
 	$('.step button.reset').click(function() {
 		$('.step input').val('');
 		var $parentStep = $(this).closest('.step');
@@ -85,20 +77,22 @@ $(function(){
 		$('.step').removeClass('active');
 		$('.progressBar .step:eq(0)').addClass('active');
 	});	
+	$(".stepBody input").on('keyup', function (e) {
+	    if (e.keyCode == 13) {
+	        $(this).next('button').click();
+	    }
+	}).on("click", function () {
+		$(this).select();
+	});
 });
-
 var costPerDay = function() {
-	totalDue = parseFloat($('#totalDue').val()) || 0.0;
-	serviceDays = parseFloat($('#serviceDays').val()) || 0.0;
-
+	var totalDue = parseFloat($('#totalDue').val()) || 0.0;
+	var serviceDays = parseFloat($('#serviceDays').val()) || 0.0;
 	return totalDue/serviceDays;
 }
-
 var amountDue = function() {
 	var dayRate = costPerDay();
-	partialDays = parseFloat($('#partialDays').val()) || 0.0;
-
+	var partialDays = parseFloat($('#partialDays').val()) || 0.0;
 	var partialDue = dayRate * partialDays;
-
 	return partialDue.toFixed(2);
 }
